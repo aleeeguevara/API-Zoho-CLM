dd139076742ae3373e5d11dbde42a9ba19b96d2e
Ale <bguevaraale@gmail.com>
2024-07-26 13:58:28 -0300

    feature(#84382): Change execution by table view

--- src/analytics/analytics.controller.ts
+++ src/analytics/analytics.controller.ts
@@ -2,45 +2,65 @@
 https://docs.nestjs.com/controllers#controllers
 */
 
-import { Body, Controller, Get, HttpException, HttpStatus, Post, Query } from '@nestjs/common';
+import { Controller, Get, HttpException, HttpStatus, Query, Post, Body } from '@nestjs/common';
 import { AnalyticsService } from './analytics.service';
 import { ApiExecuteJobData } from './types';
 
 
 @Controller('analytics')
 export class AnalyticsController {
-    constructor(public analyticsService: AnalyticsService){}
-    
+    constructor(public analyticsService: AnalyticsService) {}
+
     @Post('execute-job')
     async executeJob(
+        @Body('view')view:string,
         @Body('config')config:string
     ): Promise<ApiExecuteJobData[]> {
         try{
-            return await this.analyticsService.executeJob(config)
+            return await this.analyticsService.executeJob(view, config);
         }catch(err){
             console.error(err.message);
             throw new HttpException(err.message, HttpStatus.INTERNAL_SERVER_ERROR);
-        } 
+        }
     }
+    
     @Get('execute-job')
     async executeJobWithCriteria(
-      @Query('field') field: string,
-      @Query('fieldValue') fieldValue: string,
+        @Query('view') view: string,
+        @Query('field') field: string,
+        @Query('fieldValue') fieldValue: string
     ): Promise<ApiExecuteJobData[]> {
       try {
-        return await this.analyticsService.executeJobWithCriteria(field, fieldValue);
+        return await this.analyticsService.executeJobWithCriteria(view, field, fieldValue);
       } catch (err) {
         console.error(err.message);
         throw new HttpException(err.message, HttpStatus.INTERNAL_SERVER_ERROR);
       }
     }
-    @Get('get-all')  // The same as above, but without query parameters.
-    async executeJobWithoutCriteria():Promise<ApiExecuteJobData[]> {
+
+    @Get('get-all') // The same as above, but without query parameters.
+    async executeJobWithoutCriteria(
+        @Query('view') view: string
+    ): Promise<ApiExecuteJobData[]> {
       try {
-        return await this.analyticsService.executeJobWithoutCriteria();
+        return await this.analyticsService.executeJobWithoutCriteria(view);
       } catch (err) {
         console.error(err.message);
         throw new HttpException(err.message, HttpStatus.INTERNAL_SERVER_ERROR);
       }
+    }
+
+    @Get('execute-job-by-view')
+    async executeJobByView(
+        @Query('view') view: string,
+        @Query('field') field: string,
+        @Query('fieldValue') fieldValue: string
+    ): Promise<ApiExecuteJobData[]> {
+        try {
+            return await this.analyticsService.executeJobByView(view, field, fieldValue);
+        } catch (err) {
+            console.error(err.message);
+            throw new HttpException(err.message, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
     }
 }
--- src/analytics/analytics.service.ts
+++ src/analytics/analytics.service.ts
@@ -1,119 +1,139 @@
-/*
-https://docs.nestjs.com/providers#services
-*/
-
 import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import axios from 'axios';
-import { ApiExecuteJobData, ApiZohoAnalyticsData, CheckingData } from './types';
+import { ApiExecuteJobData, CheckingData } from './types';
 import { formatData } from '../helpers/formatting.helper';
-
 
 @Injectable()
 export class AnalyticsService {
     private clientId: string;
     private clientSecret: string;
     private refreshToken: string;
-    private workspaceId: string;
-    private viewId: string;
     
+    private viewMap = {
+        Extranet_Potenciais: {
+            workspaceId: '1121931000000364322',
+            viewId: '1121931000019729008'
+        },
+        Extranet_SalesOrders: {
+            workspaceId: '1121931000000364322',
+            viewId: '1121931000019729373'
+        },
+        Pedidos_Portal: {
+            workspaceId: '1121931000006683001',
+            viewId: '1121931000012638002'
+        }
+    };
+
     constructor(private configService: ConfigService) {
         this.clientId = this.configService.get<string>('CLIENT_ID');
         this.clientSecret = this.configService.get<string>('CLIENT_SECRET');
         this.refreshToken = this.configService.get<string>('REFRESH_TOKEN');
-        this.workspaceId = this.configService.get<string>('WORKSPACE_ID');
-        this.viewId = this.configService.get<string>('VIEW_ID');
     }
+
+    private getViewIds(view: string) {
+        const viewConfig = this.viewMap[view];
+        if (!viewConfig) {
+            throw new HttpException('Invalid view name', HttpStatus.BAD_REQUEST);
+        }
+        return viewConfig;
+    }
+
     async refreshAccessToken(): Promise<string> {
-        const url = `https://accounts.zoho.com/oauth/v2/token?refresh_token=${this.refreshToken}&client_id=${this.clientId}&client_secret=${this.clientSecret}&grant_type=refresh_token`
-        try{
-            const response = await axios.post(url)
+        const url = `https://accounts.zoho.com/oauth/v2/token?refresh_token=${this.refreshToken}&client_id=${this.clientId}&client_secret=${this.clientSecret}&grant_type=refresh_token`;
+        try {
+            const response = await axios.post(url);
             return response.data.access_token;
-        }catch(error){
+        } catch (error) {
             console.error(error.message);
             throw new HttpException('Failed to get access token', HttpStatus.UNAUTHORIZED);
         }
     }
+
     private async authenticateRequest(config, accessToken): Promise<CheckingData> {
-        config.headers= {
+        config.headers = {
             ...config.headers,
             Authorization: `Zoho-oauthtoken ${accessToken}`,
             'ZANALYTICS-ORGID': '67615980'
-        }
+        };
 
-        try{
-            const response = await axios(config)
-            return response.data
-        }catch(error){
+        try {
+            const response = await axios(config);
+            return response.data;
+        } catch (error) {
             console.error(error.message);
             throw new HttpException(error.message, HttpStatus.INTERNAL_SERVER_ERROR);
         }
     }
-    async createExportJob(config, accessToken): Promise<string> {
-        const url =`https://analyticsapi.zoho.com/restapi/v2/bulk/workspaces/${this.workspaceId}/views/${this.viewId}/data?CONFIG=${encodeURIComponent(config)}`;
+
+    async createExportJob(view: string, config: string, accessToken: string): Promise<string> {
+        const { workspaceId, viewId } = this.getViewIds(view);
+        const url = `https://analyticsapi.zoho.com/restapi/v2/bulk/workspaces/${workspaceId}/views/${viewId}/data?CONFIG=${encodeURIComponent(config)}`;
 
         const response = await this.authenticateRequest({
             url,
             method: 'GET'
-        }, accessToken)
-        return response.data.jobId
+        }, accessToken);
+        return response.data.jobId;
     }
-    async getDownloadJobUrl(jobId, accessToken):Promise<CheckingData>{
-        const url = `https://analyticsapi.zoho.com/restapi/v2/bulk/workspaces/${this.workspaceId    }/exportjobs/${jobId}`;
+
+    async getDownloadJobUrl(view: string, jobId: string, accessToken: string): Promise<CheckingData> {
+        const { workspaceId } = this.getViewIds(view);
+        const url = `https://analyticsapi.zoho.com/restapi/v2/bulk/workspaces/${workspaceId}/exportjobs/${jobId}`;
         return await this.authenticateRequest({
             url,
             method: 'GET'
-        }, accessToken)
+        }, accessToken);
     }
 
-    async exportJobData(downloadUrl, accessToken):Promise<ApiExecuteJobData[]>{
+    async exportJobData(downloadUrl: string, accessToken: string): Promise<ApiExecuteJobData[]> {
         const response = await this.authenticateRequest({
-            url:downloadUrl,
+            url: downloadUrl,
             method: 'GET'
-        }, accessToken)
-        
-        return formatData(response.data)
+        }, accessToken);
+
+        return formatData(response.data);
     }
 
-    async waitForJobCompleted(jobId: string, accessToken: string): Promise<string> {
+    async waitForJobCompleted(view: string, jobId: string, accessToken: string): Promise<string> {
         let jobStatus;
         let downloadUrl;
         do {
-            const response = await this.getDownloadJobUrl(jobId, accessToken);            
+            const response = await this.getDownloadJobUrl(view, jobId, accessToken);            
             jobStatus = response.data.jobStatus;
 
             if (jobStatus === 'JOB COMPLETED') {
-                downloadUrl = response.data.downloadUrl
-            }else if (jobStatus.includes('FAILED')){
-                throw new HttpException('Export job failed', HttpStatus.INTERNAL_SERVER_ERROR)
-            }else{
+                downloadUrl = response.data.downloadUrl;
+            } else if (jobStatus.includes('FAILED')) {
+                throw new HttpException('Export job failed', HttpStatus.INTERNAL_SERVER_ERROR);
+            } else {
                 await new Promise(resolve => setTimeout(resolve, 3000));
             }
-        }
-        while (jobStatus !== 'JOB COMPLETED');
+        } while (jobStatus !== 'JOB COMPLETED');
         return downloadUrl;
     }
 
-    async executeJob(config): Promise<ApiExecuteJobData[]>{
+    async executeJob(view: string, config: string): Promise<ApiExecuteJobData[]> {
         const accessToken = await this.refreshAccessToken();
-        const jobId = await this.createExportJob(config, accessToken);
-        const downloadUrl = await this.waitForJobCompleted(jobId, accessToken);
+        const jobId = await this.createExportJob(view, config, accessToken);
+        const downloadUrl = await this.waitForJobCompleted(view, jobId, accessToken);
         return await this.exportJobData(downloadUrl, accessToken);
     }
 
-    async executeJobWithCriteria(field: string, fieldValue: string): Promise<ApiExecuteJobData[]> {
+    async executeJobWithCriteria(view: string, field: string, fieldValue: string): Promise<ApiExecuteJobData[]> {
         const criteria = `\"${field}\"=${fieldValue}`;
         const config = JSON.stringify({ responseFormat: 'json', criteria });
-        const accessToken = await this.refreshAccessToken();
-        const jobId = await this.createExportJob(config, accessToken);
-        const downloadUrl = await this.waitForJobCompleted(jobId, accessToken);
-        return await this.exportJobData(downloadUrl, accessToken);
-      }
-    async executeJobWithoutCriteria():Promise<ApiExecuteJobData[]> {
+        return await this.executeJob(view, config);
+    }
+
+    async executeJobWithoutCriteria(view: string): Promise<ApiExecuteJobData[]> {
         const config = JSON.stringify({ responseFormat: 'json' });
-        const accessToken = await this.refreshAccessToken();
-        const jobId = await this.createExportJob(config, accessToken);
-        const downloadUrl = await this.waitForJobCompleted(jobId, accessToken);
-        return await this.exportJobData(downloadUrl, accessToken);
+        return await this.executeJob(view, config);
+    }
+
+    async executeJobByView(view: string, field: string, fieldValue: string): Promise<ApiExecuteJobData[]> {
+        const criteria = `\"${view}\".\"${field}\"='${fieldValue}'`;
+        const config = JSON.stringify({ responseFormat: 'json', criteria });
+        return await this.executeJob(view, config);
     }
 }
